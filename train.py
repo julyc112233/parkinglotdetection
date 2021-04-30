8ee2194826c9bdd8d412b0468eeec1d7534b2ff2
julyc <752787496@qq.com>
2021-04-30 18:56:25 +0800

    Initial commit
    添加ssd的数据增强模块
    
    将ssd数据增强模块适应二分类任务，并且修改了dataloader的图片处理
    
    添加了cuda支持以及cudnn，visdom可视化,visdom没有测试，可能存在bug
    
    Signed-off-by: julyc <752787496@qq.com>

--- train.py
+++ train.py
@@ -24,32 +24,30 @@ from data.cnrparkext import *
 from torch.autograd import Variable
 
 # 数据分割只跑一次，分割完记得改成False
-need_split_data=False
-
-
+need_split_data = False
 
 HOME = os.path.expanduser("~")
+# data_root=osp.join(HOME,".jupyter/cnrpark")
 # data_root=osp.join(HOME,".jupyter/cnrpark/CNRPark-Patches-150x150")
-data_root=osp.join(HOME,"Downloads/CNR-EXT-Patches-150x150")
-batch_size=64
-num_work=0
-epoch_num=50
-lr=0.0001
-momentum=0.9
-weight_decay=5e-4
-
+# data_root=osp.join(HOME,"Downloads/CNR-EXT-Patches-150x150")
+batch_size = 32
+num_work = 32
+epoch_num = 50
+lr = 0.0001
+momentum = 0.9
+weight_decay = 5e-4
 
 MEANS = (104, 117, 123)
 
-targ_root=osp.join(HOME,"PycharmProjects/vgg16/data/cnrext")
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
+
 parser = argparse.ArgumentParser(
     description='Single Shot MultiBox Detector Training With Pytorch')
 train_set = parser.add_mutually_exclusive_group()
-parser.add_argument('--dataset_root', default=data_root,
+parser.add_argument('--dataset_root', default=None,
                     help='Dataset root directory path')
 parser.add_argument('--dataset_name', default="cnrext",
                     help='Dataset root directory path')
@@ -63,7 +61,7 @@ parser.add_argument('--start_iter', default=0, type=int,
                     help='Resume training at this iter')
 parser.add_argument('--num_workers', default=4, type=int,
                     help='Number of workers used in dataloading')
-parser.add_argument('--cuda', default=False, type=str2bool,
+parser.add_argument('--cuda', default=True, type=str2bool,
                     help='Use CUDA to train model')
 parser.add_argument('--cuda_device', default=3, type=int,
                     help='specific the cuda device')
@@ -75,24 +73,34 @@ parser.add_argument('--weight_decay', default=5e-4, type=float,
                     help='Weight decay for SGD')
 parser.add_argument('--gamma', default=0.1, type=float,
                     help='Gamma update for SGD')
-parser.add_argument('--visdom', default=False, type=str2bool,
+parser.add_argument('--visdom', default=True, type=str2bool,
                     help='Use visdom for loss visualization')
 parser.add_argument('--save_folder', default='weights/',
                     help='Directory for saving checkpoint models')
 args = parser.parse_args()
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+if args.dataset_name == "cnrext":
+    targ_root = osp.join(HOME, ".jupyter/cnrpark/parkinglotdetection/data/cnrext")
+    data_root = osp.join(HOME, ".jupyter/cnrpark")
+elif args.dataset_name == "snrsmall":
+    data_root = osp.join(HOME, ".jupyter/cnrpark/CNRPark-Patches-150x150")
+    targ_root = osp.join(HOME, ".jupyter/cnrpark/parkinglotdetection/data/cnr_park_small/train/")
+# # targ_root=osp.join(HOME,"PycharmProjects/vgg16/data/cnrext")
+# # targ_root=osp.join(HOME,".jupyter/cnrpark/parkinglotdetection/data/cnrext")
+# targ_root=osp.join(HOME,".jupyter/cnrpark/parkinglotdetection/data/cnr_park_small/train/")
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 # if torch.cuda.is_available():
-# #     if args.cuda:
-# #         torch.set_default_tensor_type('torch.cuda.FloatTensor')
-# #     if not args.cuda:
-# #         print("WARNING: It looks like you have a CUDA device, but aren't " +
-# #               "using CUDA.\nRun with --cuda for optimal training speed.")
-#       torch.set_default_tensor_type('torch.FloatTensor')
+#     if args.cuda:
+#         torch.set_default_tensor_type('torch.cuda.FloatTensor')
+#     if not args.cuda:
+#         print("WARNING: It looks like you have a CUDA device, but aren't " +
+#               "using CUDA.\nRun with --cuda for optimal training speed.")
+#         torch.set_default_tensor_type('torch.FloatTensor')
 # else:
 torch.set_default_tensor_type('torch.FloatTensor')
-# from torch.multiprocessing import set_start_method
+from torch.multiprocessing import set_start_method
 
 # try:
 
@@ -102,7 +110,10 @@ torch.set_default_tensor_type('torch.FloatTensor')
 #     pass
 if args.visdom:
     import visdom
+
     viz = visdom.Visdom()
+
+
 def net_train():
     print("loading net...")
 
@@ -115,12 +126,13 @@ def net_train():
     print("change to train mode ...")
     net.train()
 
-    train_loss=0
+    train_loss = 0
     print("process_data...")
     if args.dataset_name == "cnrext":
-        dataset=cnrext(targ_root,transform=SSDAugmentation(224,MEANS))
+        dataset = cnrext(targ_root, transform=SSDAugmentation(224, MEANS))
     else:
-        if args.dataset_name=="cnrsmall":
+        if args.dataset_name == " cnrsmall":
+            print("cnrsmall")
             dataset = cnrpark_small(targ_root, transform=SSDAugmentation(224, MEANS))
     epoch_size = len(dataset) // batch_size
 
@@ -130,22 +142,21 @@ def net_train():
         iter_plot = create_vis_plot('Iteration', 'Loss', vis_title, vis_legend)
         epoch_plot = create_vis_plot('Epoch', 'Loss', vis_title, vis_legend)
 
-    data_loader=torch.utils.data.DataLoader(dataset,batch_size,num_workers=num_work,shuffle=True,pin_memory=True)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size, num_workers=num_work, shuffle=True, pin_memory=False)
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum,
                           weight_decay=weight_decay)
     batch_iterator = iter(data_loader)
-    cost=nn.CrossEntropyLoss()
-    running_loss=0.0
-    epoch=0
+    cost = nn.CrossEntropyLoss()
+    running_loss = 0.0
+    epoch = 0
     print("start train...")
 
-   
-    for iteration in range(0,50000):
-        if(iteration%epoch_size==0):
-            running_loss=0
-            epoch+=1
+    for iteration in range(0, 500000):
+        if (iteration % epoch_size == 0):
+            running_loss = 0
+            epoch += 1
         try:
-            images,targets=next(batch_iterator)
+            images, targets = next(batch_iterator)
         except StopIteration:
             pass
         # print(targets)
@@ -155,27 +166,29 @@ def net_train():
                 images = Variable(images.cuda())
                 targets = [Variable(ann.cuda()) for ann in targets]
             else:
-                images=Variable(images)
-                targets=[Variable(ann) for ann in targets]
-        t0=time.time()
-        out=net(images)
+                images = Variable(images)
+                targets = [Variable(ann) for ann in targets]
+        t0 = time.time()
+        out = net(images)
         optimizer.zero_grad()
-        loss=cost(out,targets[0])
+        loss = cost(out, targets[0])
         loss.backward()
         optimizer.step()
-        t1=time.time()
+        t1 = time.time()
 
-        running_loss+=loss.data
-        if iteration % 10==0:
-            print('timer:%.4f sec.'%(t1-t0))
-            print('iter'+repr(iteration)+'||loss:%.4f||'%(loss.item()),end=' ')
+        running_loss += loss.data
+        if iteration % 10 == 0:
+            print('timer:%.4f sec.' % (t1 - t0))
+            print('iter' + repr(iteration) + '||loss:%.4f||' % (loss.item()), end=' ')
         if args.visdom:
             update_vis_plot(iteration, loss.item(),
                             iter_plot, epoch_plot, 'append')
-        if iteration !=0 and iteration % 5000==0:
-            print('Saving state,iter:',iteration)
-            torch.save(net.state_dict,'weights/vgg16_cnrpark'+repr(iteration)+'.pth')
+        if iteration != 0 and iteration % 5000 == 0:
+            print('Saving state,iter:', iteration)
+            torch.save(net.state_dict, 'weights/vgg16_cnrparkext' + repr(iteration) + '.pth')
         # running_correct += torch.sum(out== y.data)
+
+
 # net_train()
 def create_vis_plot(_xlabel, _ylabel, _title, _legend):
     return viz.line(
@@ -188,6 +201,8 @@ def create_vis_plot(_xlabel, _ylabel, _title, _legend):
             legend=_legend
         )
     )
+
+
 def update_vis_plot(iteration, loss, window1, window2, update_type,
                     epoch_size=1):
     viz.line(
@@ -205,11 +220,17 @@ def update_vis_plot(iteration, loss, window1, window2, update_type,
             update=True
         )
 
+
 if __name__ == '__main__':
     if need_split_data:
         print("split data to train ans test...")
         # im,label=getdataset(data_root)
         # print(data_root)
-        cnrext_dataset_split(data_root,"train")
+        if args.dataset_name == "cnrext":
+            cnrext_dataset_split(data_root, "train")
+        else:
+            if args.dataset_name == "cnrsmall":
+                im, label = getdataset(data_root)
+                cnrsmall_dataset_split(im, label)
     net_train()
 
